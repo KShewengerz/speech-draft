63173839e97bf6abe59a0719f532e0389271b871
Kristy Mae Almuete <kristyalmuete@gmail.com>
2018-12-10 19:56:31 +0800

    Update Heroku Commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path    = require("path");
 const app     = express();
 const port    = process.env.PORT || 8080;
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}/dist/speech-draft`));
 
 app.listen(port);
 
-app.get("/*", (req, res) => res.sendFile(path.join(`${__dirname}/dist/index.html`)));
+app.get("/*", (req, res) => res.sendFile(path.join(`${__dirname}/dist/speech-draft/index.html`)));
 
 console.log("Console listening");
