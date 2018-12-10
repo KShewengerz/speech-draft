60be0c924b094bb48018782ceea613d7328ac56c
Kristy Mae Almuete <kristyalmuete@gmail.com>
2018-12-10 18:49:16 +0800

    Initial Heroku commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path    = require("path");
+
+const app     = express();
+const port    = process.env.PORT || 8080;
+
+app.use(express.static(`${__dirname}/dist`));
+
+app.listen(port);
+
+app.get("/*", (req, res) => res.sendFile(path.join(`${__dirname}/dist/index.html`)));
+
+console.log(`Listening to port ${port}`);
